6951087f9f68af6363c6456bc1e92708b2466264
FakhriyNugraha <seftimarni30@gmail.com>
2025-07-09 21:34:12 +0700

    day2

--- resources/views/contact.blade.php
+++ resources/views/contact.blade.php
@@ -1,22 +1,34 @@
 <!DOCTYPE html>
-<html lang="en">
+<html lang="en" class="h-full bg-gray-100">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Our Contact</title>
-    <a href="/">home</a>
-    <a href="/about">about</a>
-    <a href="/blog">blog</a>
+    <script defer src="https://cdn.jsdelivr.net/npm/alpinejs@3.x.x/dist/cdn.min.js"></script>
+        @vite('resources/css/app.css')
+    <link rel="stylesheet" href="https://rsms.me/inter/inter.css" />
+    <title>halaman contact</title>
 </head>
-<h1>Hubungi Kami!</h1>
-<body>
-    <p>
+<body class="h-full">
+
+<div class="min-h-full">
+  <x-navbar></x-navbar>
+
+  <x-header>Halaman Contact</x-header>
+  <main>
+    <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+      <h1>Hubungi Kami!</h1>
+<p>
         <img src="img/ig.jpeg" alt="foto ig" width="20"> : <a href="https://www.instagram.com/nugrahafakhriy?igsh=encwandrdno2YnAx">@nugrahafakhriy</a>
         
     </p>
     <p>
         <img src="img/fb.png" alt="foto ig" width="20"> : <a href="https://www.facebook.com/fakhriy.nugrha">Fakhriy Nugraha</a>
     </p>
+
+    </div>
+  </main>
+</div>
+
 </body>
 </html>

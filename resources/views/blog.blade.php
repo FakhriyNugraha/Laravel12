92702a2804576286008b1dd5740d8ab34131863f
FakhriyNugraha <seftimarni30@gmail.com>
2025-07-09 19:04:40 +0700

    first commit

---
+++ resources/views/blog.blade.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Blog</title>
+    <a href="/">home</a>
+    <a href="/about">about</a>
+    <a href="/contact">contact</a>
+</head>
+<h1>Siapa itu Sungjinwoo ?</h1>
+<img src="img/sjw.jpeg" alt="foto sjw" width="180">
+<body>
+  <p align="justify"> Sung Jin‑woo adalah tokoh utama dari serial Solo Leveling, sebuah webnovel, manhwa, dan anime yang sangat populer karya Chugong. Pada awal cerita, Jin‑woo dikenal sebagai Hunter peringkat E yang paling lemah, bahkan sering dianggap beban oleh rekan-rekannya.
+     Namun hidupnya berubah drastis setelah insiden di sebuah "double dungeon" misterius, di mana ia hampir mati. Di titik kritis itu, ia terpilih oleh sebuah entitas bernama System, sebuah program supernatural yang memungkinkannya "naik level" layaknya karakter dalam game. Seiring berjalannya waktu, Jin‑woo menjadi semakin kuat dan akhirnya mewarisi kekuatan Shadow Monarch, penguasa bayangan yang mampu membangkitkan dan mengendalikan pasukan undead dari bayangan musuh yang dikalahkannya. Perjalanannya yang dimulai dari titik terendah menuju puncak kekuatan menjadikan Jin‑woo sebagai simbol kekuatan dan tekad. Ia memiliki sifat yang tenang, rendah hati, dan sangat melindungi keluarganya, tetapi menjadi sangat ganas dan tak kenal ampun saat bertarung. Dalam perkembangan cerita, Jin‑woo menjadi kunci pertahanan umat manusia dari ancaman para Monarch dan kekuatan destruktif dari dunia lain. Di akhir kisah, ia menikah dengan Cha Hae‑in dan memiliki seorang putra bernama Suho. Karakter Jin‑woo sangat dicintai penggemar karena transformasinya yang luar biasa dan perjuangannya melawan takdir yang awalnya tampak mustahil untuk diubah.</p>
+
+     <h1>Siapa itu Cha Hae In?</h1>
+    <img src="img/chin.jpeg" alt="foto cha hae in" width="180">
+     <p align="justify">Cha Hae-In adalah salah satu karakter utama dalam cerita Solo Leveling, yang dikenal sebagai satu-satunya Hunter perempuan peringkat S di Korea Selatan. Ia merupakan wakil pemimpin dari Guild Hunters dan memiliki kemampuan bertarung luar biasa, terutama dalam seni pedang. Julukannya, “The Dancer,” berasal dari gaya bertarungnya yang anggun dan efisien, seolah sedang menari saat bertempur. Salah satu ciri khasnya adalah kemampuan unik untuk mencium bau aura seseorang—di mana sebagian besar Hunter dan monster tercium seperti bau busuk, namun hanya Sung Jin-Woo yang baginya memiliki aroma yang "menyenangkan", yang kemudian memicu rasa penasaran dan ketertarikan padanya.
+        Cha Hae-In digambarkan sebagai sosok yang tenang, disiplin, dan sangat bertanggung jawab. Meski berada di puncak peringkat Hunter, ia tetap rendah hati, rajin berlatih, dan menunjukkan kepedulian tinggi terhadap rekan maupun masyarakat sipil. Dalam perkembangan cerita, hubungan antara Cha Hae-In dan Sung Jin-Woo perlahan tumbuh dari rasa hormat profesional menjadi cinta yang mendalam, terlebih setelah mereka menghadapi berbagai bahaya bersama. Di akhir cerita utama, mereka menikah dan memiliki seorang putra bernama Suho. Sosok Cha Hae-In tidak hanya menjadi pendamping bagi Jin-Woo, tetapi juga simbol kekuatan, keberanian, dan keteguhan hati seorang pejuang wanita sejati.</p>
+
+</body>
+</html>
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Our Contact</title>
+    <a href="/">home</a>
+    <a href="/about">about</a>
+    <a href="/blog">blog</a>
+</head>
+<h1>Hubungi Kami!</h1>
+<body>
+    <p>
+        <img src="img/ig.jpeg" alt="foto ig" width="20"> : <a href="https://www.instagram.com/nugrahafakhriy?igsh=encwandrdno2YnAx">@nugrahafakhriy</a>
+        
+    </p>
+    <p>
+        <img src="img/fb.png" alt="foto ig" width="20"> : <a href="https://www.facebook.com/fakhriy.nugrha">Fakhriy Nugraha</a>
+    </p>
+</body>
+</html>
